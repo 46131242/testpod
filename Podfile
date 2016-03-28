200244d7c43cd5553953e8059931bb6b801806cf
修观 <xiuguan.gp@alibaba-inc.com>
2016-03-28 13:47:44 +0800

    test

---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios,  '6.0'
+pod 'MagicalRecord', :git => 'https://github.com/iiiyu/MagicalRecord.git', :tag => 'sumiGridDiary2.1'
---
+++ testpp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testpp
+//
+//  Created by inzaghi on 16/3/28.
+//  Copyright © 2016年 inzaghi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
